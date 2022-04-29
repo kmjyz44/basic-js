e5f11d1f1ea58cc9115e114b4c3c8eb8fb51493d
kmjyz44 <kmjyz44@gmail.com>
2022-04-29 23:51:32 +0300

    dreamteam

--- src/dream.js
+++ src/dream.js
@@ -1,10 +1,12 @@
 function createDreamTeam (array){
     let secret =[];
 for(let arrays of array){
+    if (typeof arrays === 'string'){
     for(let i =0; i< arrays.length;i++){
+        
         if(arrays[i]===arrays[i].toUpperCase()){
             secret.push(arrays[i]);
-
+        }
     }
 }
 
