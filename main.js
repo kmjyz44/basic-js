2dae3860fa85d1ae7940f9dab164e99c2d11519f
kmjyz44 <kmjyz44@gmail.com>
2022-04-29 23:08:00 +0300

    task cat

---
+++ main.js
@@ -0,0 +1,17 @@
+function countCats(array) {
+    let cat =0;
+    for(let arrays of array){
+for(let i = 0; i < arrays.length;i++){
+if(arrays[i]==='^^'){
+    cat= cat+1;
+
+} 
+}
+
+ }
+ return cat;
+ 
+} 
+
+ 
+
