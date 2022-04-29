47e9ea609adbcc82460cc296840f98b78c339538
kmjyz44 <kmjyz44@gmail.com>
2022-04-29 23:44:48 +0300

    dreamteam

--- src/main.js
+++ src/main.js
@@ -14,4 +14,16 @@ if(arrays[i]==='^^'){
 } 
 
  
+function createDreamTeam (array){
+    let secret =[];
+for(let arrays of array){
+    for(let i =0; i< arrays.length;i++){
+        if(arrays[i]===arrays[i].toUpperCase()){
+            secret.push(arrays[i]);
 
+    }
+}
+
+}
+return((secret.sort()).join(''));
+}
