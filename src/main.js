404f1a2c45cbdbfe6004ce770fbd9fad5fb2438d
kmjyz44 <kmjyz44@gmail.com>
2022-04-29 23:53:21 +0300

    dreamteam

--- src/main.js
+++ src/main.js
@@ -1,17 +1,3 @@
-function countCats(array) {
-    let cat =0;
-    for(let arrays of array){
-for(let i = 0; i < arrays.length;i++){
-if(arrays[i]==='^^'){
-    cat= cat+1;
-
-} 
-}
-
- }
- return cat;
- 
-} 
 
 function createDreamTeam (array){
     let secret =[];
